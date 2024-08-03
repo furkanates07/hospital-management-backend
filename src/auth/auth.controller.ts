a7bb1359c6f4e932ca9e43ddbbf2bfdc58e7e4ec
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-03 22:43:13 +0300

    feat: add register doctor function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { DoctorsService } from 'src/doctors/doctors.service';
+import { CreateDoctorDto } from 'src/doctors/dto';
 import { CreatePatientDto } from 'src/patients/dto';
 import { PatientsService } from 'src/patients/patients.service';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
+import { Role } from './enums/role';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
@@ -10,6 +13,7 @@ export class AuthController {
   constructor(
     private authService: AuthService,
     private patientsService: PatientsService,
+    private doctorsService: DoctorsService,
   ) {}
 
   @Post('register')
@@ -24,10 +28,23 @@ export class AuthController {
     @Req() req: any,
   ) {
     const user = req.user;
-    if (user.role !== 'doctor') {
+    if (user.role !== Role.DOCTOR) {
       throw new Error('Only doctors can register patients');
     }
     return this.patientsService.createPatient(createPatientDto);
+  }
+
+  @Post('register-doctor')
+  @UseGuards(JwtAuthGuard)
+  async registerDoctor(
+    @Body() createDoctorDto: CreateDoctorDto,
+    @Req() req: any,
+  ) {
+    const user = req.user;
+    if (user.role !== Role.ADMIN) {
+      throw new Error('Only admins can register doctors');
+    }
+    return this.doctorsService.createDoctor(createDoctorDto);
   }
 
   @Post('login')
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DoctorsModule } from 'src/doctors/doctors.module';
 import { PatientsModule } from 'src/patients/patients.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -13,6 +14,7 @@ import { JwtStrategy } from './strategy/jwt.strategy';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.register({ secret: 'cat' }),
     PatientsModule,
+    DoctorsModule,
   ],
   providers: [AuthService, JwtStrategy, JwtAuthGuard],
   controllers: [AuthController],
