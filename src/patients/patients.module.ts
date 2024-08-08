6bfc2b989ea63208137f450cb99225603c6654fb
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-08 20:11:40 +0300

    feat: import auth module

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 import { Patient, PatientSchema } from './schemas/patient.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),
+    forwardRef(() => AuthModule),
   ],
   controllers: [PatientsController],
   providers: [PatientsService],
