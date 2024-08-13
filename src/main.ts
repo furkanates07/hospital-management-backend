bfc15e7b3ba000bae2cd1523ce52d9312d31e20f
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-13 20:33:06 +0300

    feat: check mongoose connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import mongoose, { ConnectOptions } from 'mongoose';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dbURI = process.env.DB_URI;
+
+  await mongoose.connect(dbURI, { useUnifiedTopology: true } as ConnectOptions);
+  console.log('MongoDB connection successful!');
+
   await app.listen(3000);
+  console.log('NestJS application is running on port 3000!');
 }
 bootstrap();
