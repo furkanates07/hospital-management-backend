9d4e8311ca0deadda5988c9058899a4038ec080d
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-01 20:21:47 +0300

    feat: add patch method

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
