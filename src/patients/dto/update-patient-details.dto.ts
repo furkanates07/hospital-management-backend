486d77aeddcc3d309b8d22ab28b169a5d6e47693
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-10 11:59:18 +0300

    chore: change import orders

--- src/patients/dto/update-patient-details.dto.ts
+++ src/patients/dto/update-patient-details.dto.ts
@@ -1,6 +1,6 @@
+import { Type } from 'class-transformer';
 import { IsEmail, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { EmergencyContact } from '../schemas/emergency-contact.schema';
-import { Type } from 'class-transformer';
 
 export class UpdatePatientDetailsDto {
   @IsOptional()
