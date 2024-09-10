494310b3f386a103db411117b7f2f6c8d366896f
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-10 12:00:32 +0300

    feat: add new dto for password change

--- src/patients/dto/index.ts
+++ src/patients/dto/index.ts
@@ -1,3 +1,4 @@
+export { ChangePasswordDto } from './change-password.dto';
 export { CreatePatientDto } from './create-patient.dto';
 export { UpdatePatientConditionsDto } from './update-patient-conditions.dto';
 export { UpdatePatientDetailsDto } from './update-patient-details.dto';
